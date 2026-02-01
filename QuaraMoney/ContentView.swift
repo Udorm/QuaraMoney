a738e5bad79ec746b1d2564608c484e043f87abd
udorm <phonudorm@gmail.com>
2026-02-01 11:54:05 +0700

    Initial Commit

---
+++ QuaraMoney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuaraMoney
+//
+//  Created by Udorm Phon on 01-02-2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuaraMoney/QuaraMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuaraMoneyApp.swift
+//  QuaraMoney
+//
+//  Created by Udorm Phon on 01-02-2026.
+//
+
+import SwiftUI
+
+@main
+struct QuaraMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
